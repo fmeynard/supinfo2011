e9f73faf960b169cc7ccc1edad85df1cedb57961
Maldus <murasylvain@hotmail.com>
2011-06-17 19:40:29 +0100

    Clean Forum // Forum Finished // Commentaire to do // + Mise en page création de topic

--- apps/forum/templates/layout.php
+++ apps/forum/templates/layout.php
@@ -1,183 +1,159 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
-<title>Forum SafeDriving</title>
-    <?php include_stylesheets() ?>
-    <?php include_javascripts() ?>
+    <head>
+        <?php include_http_metas() ?>
+        <?php include_metas() ?>
+        <?php include_title() ?>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Forum SafeDriving</title>
+        <?php include_stylesheets() ?>
+        <?php include_javascripts() ?>
 
-  </head>
- <body class="ltr">
-<div class="wrapperbg">
-<a name="top"></a>
+    </head>
+    <body class="ltr">
+        <div class="wrapperbg">
+            <a name="top"></a>
 
+            <div class="logo">
+                <div class="logo-left"><a href=""><img src="/images/forum/lclogo.png" alt="" /></a></div>
+                <div class="logo-right"></div>
+                <div class="clear"></div>
+            </div>
 
-<div class="logo">
+            <div class="topbarnav">
+                <div class="topbarleft">
+                    <div class="topbar-left-cap"></div>
+                    <ul class="topmenuleft">
+                        <li class="home"> <a href="<?php echo url_for('forum/index') ?>">Home</a></li>
+                        <li> <a href="">Contact</a></li>
+                    </ul>
+                    <div class="topbar-right-cap"></div>
+                </div>
 
-<div class="logo-left"><a href=""><img src="/images/forum/lclogo.png" alt="" /></a></div>
+                <div class="topbarright">
+                    <div class="topbar-left-cap2"></div>
+                    <ul class="topmenuright">
 
-<div class="logo-right"></div>
-
-<div class="clear"></div>
-
-
-</div>
-<div class="topbarnav">
-
-
-<div class="topbarleft">
-<div class="topbar-left-cap"></div>
-<ul class="topmenuleft">
-<li class="home"> <a href="<?php echo url_for('forum/index') ?>">Home</a></li>
-<li> <a href="">Contact</a></li>
-</ul>
-<div class="topbar-right-cap"></div>
-</div>
-
-<div class="topbarright">
-<div class="topbar-left-cap2"></div>
-<ul class="topmenuright">
-
-       <?php if($sf_user->isAuthenticated()): ?>
-
-                         <li class="home">Welcome <?php echo $sf_user->getProfile()->getFullname(); ?></li>
-                  <li>  <?php echo link_to("Log out","sfGuardAuth/signout",array("class"=>"user_area")) ?></li>
-                        <?php else:?>
-                      
-                  <li class="home">   <?php echo link_to("Log In","sfGuardAuth/signin",array("class"=>"user_area")) ?> </li>
-                  <li>   <?php echo link_to("Register","sfGuardAuth/register",array("class"=>"user_area")) ?> </li>
+                        <?php if ($sf_user->isAuthenticated()): ?>
+                            <li class="home">Welcome <?php echo $sf_user->getProfile()->getFullname(); ?></li>
+                            <li>  <?php echo link_to("Log out", "sfGuardAuth/signout", array("class" => "user_area")) ?></li>
+                        <?php else: ?>
+                                <li class="home">   <?php echo link_to("Log In", "sfGuardAuth/signin", array("class" => "user_area")) ?> </li>
+                                <li>   <?php echo link_to("Register", "sfGuardAuth/register", array("class" => "user_area")) ?> </li>
                         <?php endif; ?>
 
-                
+                            </ul>
+                        </div>
+                        <div class="topbar-right-cap2"></div>
+                    </div>
 
-</ul>
-</div>
-<div class="topbar-right-cap2"></div>
-</div>
+                    <div class="clear"></div>
 
-<div class="clear"></div>
+                    <table class="wrap" width="100%" cellpadding="0" cellspacing="0">
+                        <tbody><tr><td><div id="datebar">
+                                        <table width="100%" cellspacing="0">
 
-<table class="wrap" width="100%" cellpadding="0" cellspacing="0">
-<tbody><tr><td><div id="datebar">
-	  <table width="100%" cellspacing="0">
+                                            <tbody><tr>
+                                                    <td class="breadcrumbs"><?php include_slot('breadcrumb') ?></td>
+                                                    <td class="gensmall" align="right">It is currently <?php echo Date('D d M  H:i:s') ?><br /></td>
+                                                </tr>
+                                            </tbody></table>
+                                    </div>
 
-            <tbody><tr>
-			<td class="breadcrumbs"><?php include_slot('breadcrumb') ?></td>
-			<td class="gensmall" align="right">It is currently <?php echo Date('D d M  H:i:s') ?><br /></td>
-		</tr>
-		</tbody></table>
-	</div>
+                                    <br style="clear: both;"/>
 
+                            <?php if ($sf_user->isAuthenticated()): ?>
+                                    <div class="bc">
+                                        <table class="bcbg" width="100%" cellspacing="1" cellpadding="0" style="margin-top: 5px;">
+                                            <tbody><tr>
+                                                    <td class="row1">
+                                                        <p class="breadcrumbs">
+                                                    <?php if ($sf_user->getGuardUser()->getIsSuperAdmin()): ?><a href="<?php echo url_for('forum/new') ?>"> Add Category </a>  <?php endif; ?> |
+                                                    <?php if ($sf_user->getGuardUser()->getIsSuperAdmin()): ?><a href="<?php echo url_for('forumEdit/new') ?>"> Add Forum </a>  <?php endif; ?> |
+                                                    <?php if ($sf_user->getGuardUser()->getIsSuperAdmin()): ?> <a href="<?php echo url_for('forum/listEdit') ?>">Edit Categories </a><?php endif; ?>   |
+                                                    <?php if ($sf_user->getGuardUser()->getIsSuperAdmin()): ?> <a href="<?php echo url_for('forumEdit/index') ?>">Edit Forums </a><?php endif; ?>
+                                                                </p>
+                                                                <p class="datetime">  </p>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody></table>
+                                            </div>
+                            <?php endif; ?>
+                                                                    <br/>
 
-	
+                            <?php echo $sf_content ?>
+                                                                    <br />
+                                                                    <div class="genspace">
+                                                                    </div>
+                                                                    <br clear="all" />
 
+                                                                    <div class="bc">
+                                                                        <table class="bcbg" width="100%" cellspacing="1" cellpadding="0" style="margin-top: 5px;">
+                                                                            <tr>
+                                                                                <td class="row1">
+                                                                                    <p class="breadcrumbs"><a href="<?php echo url_for('forum/index') ?>">Board Index</a></p>
+                                            <p class="datetime">All members of our team are here to helps you!</p>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
 
-	<br style="clear: both;"/>
-        
-<?php if($sf_user->isAuthenticated()): ?>
-	<div class="bc">
-	<table class="bcbg" width="100%" cellspacing="1" cellpadding="0" style="margin-top: 5px;">
-	<tbody><tr>
-		<td class="row1">
-			<p class="breadcrumbs">
- <?php if($sf_user->getGuardUser()->getIsSuperAdmin()): ?><a href="<?php echo url_for('forum/new') ?>"> Add Category </a>  <?php endif; ?> |
- <?php if($sf_user->getGuardUser()->getIsSuperAdmin()): ?><a href="<?php echo url_for('forumEdit/new') ?>"> Add Forum </a>  <?php endif; ?> |
-<?php if($sf_user->getGuardUser()->getIsSuperAdmin()): ?> <a href="<?php echo url_for('forum/listEdit') ?>">Edit Categories </a><?php endif; ?>   |
-<?php if($sf_user->getGuardUser()->getIsSuperAdmin()): ?> <a href="<?php echo url_for('forumEdit/index') ?>">Edit Forums </a><?php endif; ?>
-                        </p>
-                        <p class="datetime">  </p>
-		</td>
-	</tr>
-	</tbody></table>
-</div>
-<?php endif; ?>
-	<br/>
+                            <br clear="all" />
 
+                            <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td width="18" height="18" class="tl"></td>
+                                    <td height="18" style="background:url('/images/forum/tm.png');"></td>
+                                    <td width="18" height="18" class="tr"></td>
+                                </tr>
+                                <tr>
+                                    <td width="18" class="ml"></td>
+                                    <td>
+                                    </td>
+                                    <td width="18" class="mr"></td>
+                                </tr>
+                                <tr>
+                                    <td width="18" height="18" class="bl"></td>
+                                    <td height="18" style="background:url('/images/forum/bm.png');"></td>
+                                    <td width="18" height="18" class="br"></td>
+                                </tr>
+                            </table>
 
-    <?php echo $sf_content ?>
-<br />
-        <div class="genspace">
+                            <br clear="all" />
 
+                            <table class="legend">
+                                <tr>
+                                    <td width="20" align="center"><img src="/images/forum/forum_unread.png" width="68" height="68" alt="Unread posts" title="Unread posts" /></td>
+                                    <td><span class="gensmall">New post</span></td>
+                                    <td>&nbsp;&nbsp;</td>
+                                    <td width="20" align="center"><img src="/images/forum/forum_read.png" width="68" height="68" alt="No unread posts" title="No unread posts" /></td>
+                                    <td><span class="gensmall">Already Read</span></td>
+                                    <td>&nbsp;&nbsp;</td>
+                                    <td width="20" align="center"><img src="/images/forum/forum_read_locked.png" width="68" height="68" alt="No unread posts [ Locked ]" title="No unread posts [ Locked ]" /></td>
+                                    <td><span class="gensmall">Blocked Post</span></td>
+                                </tr>
+                            </table>
 
+                            <br clear="all" />
 
-</div>
+                            <div id="footer">
 
-<br clear="all" />
+                                <span class="copyright">Forum developped for SafeDriving: <a href="">By fabrisy</a><br />
+                                </span>
+                            </div>
 
-<div class="bc">
-	<table class="bcbg" width="100%" cellspacing="1" cellpadding="0" style="margin-top: 5px;">
-	<tr>
-		<td class="row1">
-			<p class="breadcrumbs"><a href="<?php echo url_for('forum/index') ?>">Board Index</a></p>
-			<p class="datetime">All members of our team are here to helps you!</p>
-		</td>
-	</tr>
-	</table>
-</div>
+                        </td></tr>
+                </tbody>
+            </table>
 
-<br clear="all" />
+            <br clear="all" />
 
+            <div class="bmenu"><a href="#">Go to the blog</a> | <a href="#top">Back to Top</a><br />
+                <div class="sndnav">
 
-
-
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-  <tr>
-    <td width="18" height="18" class="tl"></td>
-    <td height="18" style="background:url('/images/forum/tm.png');"></td>
-    <td width="18" height="18" class="tr"></td>
-  </tr>
-  <tr>
-    <td width="18" class="ml"></td>
-    <td>
-        	</td>
-    <td width="18" class="mr"></td>
-  </tr>
-  <tr>
-    <td width="18" height="18" class="bl"></td>
-    <td height="18" style="background:url('/images/forum/bm.png');"></td>
-    <td width="18" height="18" class="br"></td>
-  </tr>
-</table>
-
-
-<br clear="all" />
-
-<table class="legend">
-<tr>
-	<td width="20" align="center"><img src="/images/forum/forum_unread.png" width="68" height="68" alt="Unread posts" title="Unread posts" /></td>
-	<td><span class="gensmall">New post</span></td>
-	<td>&nbsp;&nbsp;</td>
-	<td width="20" align="center"><img src="/images/forum/forum_read.png" width="68" height="68" alt="No unread posts" title="No unread posts" /></td>
-	<td><span class="gensmall">Already Read</span></td>
-	<td>&nbsp;&nbsp;</td>
-	<td width="20" align="center"><img src="/images/forum/forum_read_locked.png" width="68" height="68" alt="No unread posts [ Locked ]" title="No unread posts [ Locked ]" /></td>
-	<td><span class="gensmall">Blocked Post</span></td>
-</tr>
-</table>
-
-<br clear="all" />
-
-<div id="footer">
-
-	<span class="copyright">Forum developped for SafeDriving: <a href="">By fabrisy</a><br />
-	</span>
-</div>
-
-</td></tr>
-</tbody>
-</table>
-
-<br clear="all" />
-
-<div class="bmenu"><a href="#">Go to the blog</a> | <a href="#top">Back to Top</a><br />
-<div class="sndnav">
-
-	<br />
-			</div>
-</div>
-</div>
-  </body>
+                    <br />
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
--- lib/form/TopicForm.php
+++ lib/form/TopicForm.php
@@ -8,50 +8,45 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class TopicForm extends sfForm
-{
-  public function setup()
-  {
-     parent::configure();
-     $this->disableLocalCSRFProtection();
-     
-     $this->embedForm('topic', new FrmTopicForm(null, array('forum'=>$this->getOption('forum'))));
-     
-     $postForm = new FrmPostForm();
-     unset($postForm['title']);
-     $this->embedForm('post', $postForm);
-     
-     $this->widgetSchema->setNameFormat('maintopic[%s]');
-  }
-  
-  public function bind(array $taintedValues = null, array $taintedFiles = null)
-  { 
-    foreach($this->embeddedForms as $name => $form)
-    {
-      $form->bind($taintedValues[$name]);
+class TopicForm extends sfForm {
+
+    public function setup() {
+        parent::configure();
+        $this->disableLocalCSRFProtection();
+
+        $this->embedForm('topic', new FrmTopicForm(null, array('forum' => $this->getOption('forum'))));
+
+        $postForm = new FrmPostForm();
+        unset($postForm['title']);
+        $this->embedForm('post', $postForm);
+
+        $this->widgetSchema->setNameFormat('maintopic[%s]');
     }
-  }
-  
-  public function isValid()
-  {
-    foreach($this->embeddedForms as $name => $form)
-    {
-      if(!$form->isValid())
-      {
-        return false;
-      }
+
+    public function bind(array $taintedValues = null, array $taintedFiles = null) {
+        foreach ($this->embeddedForms as $name => $form) {
+            $form->bind($taintedValues[$name]);
+        }
     }
-    return true;
-  }
-  
-  public function save($con = null)
-  {
-    $topicForm  = $this->embeddedForms['topic'];
-    $postForm   = $this->embeddedForms['post'];
-    
-    $topicForm->save($con);
-    $postForm->getObject()->setTopicId($topicForm->getObject()->getId());
-    $postForm->save($con);
-  }
+
+    public function isValid() {
+        foreach ($this->embeddedForms as $name => $form) {
+            if (!$form->isValid()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function save($con = null) {
+        $topicForm = $this->embeddedForms['topic'];
+        $postForm = $this->embeddedForms['post'];
+
+        $topicForm->save($con);
+        $postForm->getObject()->setTopicId($topicForm->getObject()->getId());
+        $postForm->save($con);
+    }
+
 }
+
 ?>
