3f66e50d0c852940a205efb43dce1d2c38af86fc
Fabien MEYNARD <fabien.meynard@supinfo.com>
2011-06-19 21:21:23 +0100

    divers

--- apps/crm/modules/user/templates/_subMenu.php
+++ apps/crm/modules/user/templates/_subMenu.php
@@ -1,7 +1,3 @@
 <div id="subMenu">
-  <ul>
-    <li><?php echo link_to(__('Employees'),'@employee')?></li>
-    <li>Customers</li>
-    <li>Teachers</li>
   </ul>
 </div>
--- apps/crm/templates/_menu.php
+++ apps/crm/templates/_menu.php
@@ -5,21 +5,21 @@
       <?php echo image_tag('/images/crm/res/dashboard.png'); ?>
       <span><?php echo link_to(__('Dashboard'),'@homepage')?></span>
     </li>
-    <li<?php if($sf_params->get('module') == 'user') : ?> class="current"<?php endif; ?>>
+    <li<?php if($sf_params->get('module') == 'user' || $sf_params->get('module') == 'sfGuardUser') : ?> class="current"<?php endif; ?>>
       <?php echo image_tag('/images/crm/res/users.png'); ?>
-      <span><?php echo link_to(__('Users'),'user/index')?></span>
+      <span><?php echo link_to(__('Users'),'sfGuardUser/index')?></span>
     </li>
     <li<?php if($sf_params->get('module') == 'vehicle') : ?> class="current"<?php endif; ?>>
       <?php echo image_tag('/images/crm/res/vehicles.png'); ?>
       <span><?php echo link_to(__('Vehicles'),'vehicle/index')?></span>
     </li>
-    <li <?php if($sf_params->get('module') == 'agency') : ?> class="current"<?php endif; ?>>
+    <li <?php if($sf_params->get('module') == 'agency' || $sf_params->get('module') == 'user' || $sf_params->get('module') == 'formationCenter' || $sf_params->get('module') == 'formationSession' || $sf_params->get('module') == 'exam' || $sf_params->get('agencyRoom') == 'agency' || $sf_params->get('module') == 'vehicle') : ?> class="current"<?php endif; ?>>
       <?php echo image_tag('/images/crm/res/agency.png')?>
       <span><?php echo link_to(__('Agency'), 'agency/index')?></span>
     </li>
-    <li <?php if($sf_params->get('module') == 'category') : ?> class="current"<?php endif; ?>>
+    <li <?php if($sf_params->get('module') == 'offers') : ?> class="current"<?php endif; ?>>
       <?php echo image_tag('/images/crm/res/category.png')?>
-      <span><?php echo link_to(__('Category'), 'category/index')?></span>
+      <span><?php echo link_to(__('Business Offers'), 'offers/index')?></span>
     </li>
     <li <?php if($sf_params->get('module') == 'settings') : ?> class="current"<?php endif; ?>>
       <?php echo image_tag('/images/crm/res/settings.png'); ?>
